94d5e7be017df96b5667dc0033aa6b1e0692bfaf
shiyangyang <shiyangyang@flowpp.com>
2024-04-19 19:43:46 +0800

    删除Bun.serve相关代码，Bun环境下使用 node代码

--- src/server-engine/bun/index.ts
+++ src/server-engine/bun/index.ts
@@ -1,14 +1,1 @@
-import { AppOptions, HttpRequest, HttpResponse, TemplatedApp } from 'uWebSockets.js'
-import { BunRequest } from './request';
-import { BunTemplatedApp } from './app';
-
-export const App = (): TemplatedApp => new BunTemplatedApp()
-export const SSLApp = (options?: AppOptions): TemplatedApp => new BunTemplatedApp(options)
-export const parseBody = async (req: HttpRequest, resp: HttpResponse) => {
-    const result = await (req as BunRequest).req.arrayBuffer()
-    return Buffer.from(result)
-}
-export const getHeader = (name: string, req: HttpRequest) => {
-    const request = (req as BunRequest).req
-    return request.headers.get(name)
-}
+export * from '../native'
